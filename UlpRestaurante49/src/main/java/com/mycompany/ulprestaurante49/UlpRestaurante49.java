1c8afc54ab37592b577d5c2a3959923a19bf5e45
Yuzu <yuzupoonok@gmail.com>
2023-10-05 11:06:59 -0300

    comienzo

---
+++ UlpRestaurante49/src/main/java/com/mycompany/ulprestaurante49/UlpRestaurante49.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ulprestaurante49;
+
+
+public class UlpRestaurante49 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
